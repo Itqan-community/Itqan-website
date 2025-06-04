2be296c8844c5acf889180876e4861bebfa4363d
Amr Bendary <amr.bendary.business@gmail.com>
2025-06-04 04:17:25 +0300

    move to next.js instead of astro

---
+++ app/components/NavbarLink.tsx
@@ -0,0 +1,33 @@
+"use client";
+import Link from "next/link";
+import { useTranslations } from "next-intl";
+import { usePathname } from "next/navigation";
+
+interface NavbarLinkProps {
+  context: {
+    href: string;
+    text: string;
+  };
+  locale: string;
+}
+
+export default function NavbarLink({
+  context: { href, text },
+  locale,
+}: NavbarLinkProps) {
+  const t = useTranslations("nav");
+  const pathname = usePathname();
+
+  return (
+    <Link
+      href={`/${locale}${href}`}
+      className={`${
+        pathname === `/${locale}${href}`
+          ? "text-green-600 underline underline-offset-2"
+          : "text-green-800"
+      } hover:text-green-700 font-semibold`}
+    >
+      {t(text)}
+    </Link>
+  );
+}
