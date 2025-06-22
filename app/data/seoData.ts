7a43a07ac6bf6f4bdfca4ee225991ca27aef8f3c
Amr Bendary <amr.bendary.business@gmail.com>
2025-06-22 16:30:29 +0300

    fix social preview image url

--- app/data/seoData.ts
+++ app/data/seoData.ts
@@ -5,7 +5,7 @@ export const seoData = {
       'We aim to build the largest community for developing open-source Quran technologies And improve the user experience to serve Muslims around the world.',
     url: 'https://itqan.dev/en/',
     image:
-      '/og-en.png',
+      'https://opengraph.b-cdn.net/production/images/9a5ba497-a818-439e-8ec7-cd0b39559aaf.png?token=s5M6HyA-3T4CyrSGh22syVxc_WCF6vJx6cMChxImgpU&height=630&width=1200&expires=33286319708',
   },
   ar: {
     title: 'إتقان | مجتمع تطوير تقنيات القرآن',
@@ -13,6 +13,6 @@ export const seoData = {
       'نهدف لبناء أكبر مجتمع لتطوير تقنيات القرآن الكريم مفتوحة المصدر وتحسين تجربة الاستخدام لخدمة المسلمين حول العالم',
     url: 'https://itqan.dev/ar/',
     image:
-      '/og-ar.png',
+      'https://opengraph.b-cdn.net/production/images/24f3ccc8-f60a-47ce-a95d-bcc9afdf12e0.png?token=PhACL4wnmnsbd1O7du5h2wtpTICdeSQI0X6flfPqMG0&height=630&width=1200&expires=33286317738',
   },
 }
