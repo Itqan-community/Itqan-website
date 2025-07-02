59263e7443c3a3f8a6a8cd7957164009efcb1ebb
Abdulrahman AlShayea <an.alshayea@gmail.com>
2025-07-02 08:26:06 +0300

    Update page.tsx
    إضافة أداتين

--- app/[locale]/tools/page.tsx
+++ app/[locale]/tools/page.tsx
@@ -79,6 +79,48 @@ export default function Tools({ params }: { params: { locale: string } }) {
               </div>
             </div>
           </Link>
+                              {/* MP3 Quran */}
+          <Link 
+            href={t("mp3quran.link")} 
+            target="_blank" 
+            className="flex flex-col gap-4 p-4 sm:p-6 rounded-xl bg-white hover:shadow-xl shadow-neutral-200 transition-all duration-300 hover:-translate-y-1"
+          >
+            <div className="flex flex-col">
+              <h4 className="text-lg sm:text-xl font-bold mb-2">{t("Quranpedia.title")}</h4>
+              <p className="text-neutral-600 text-sm sm:text-base font-medium leading-relaxed mb-4">
+                {t("mp3quran.description")}
+              </p>
+              <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between text-emerald-900 gap-2">
+                <div className="flex items-center gap-2">
+                  <span className="text-sm text-neutral-500">{t("author")}:</span>
+                  <span className="text-sm text-neutral-500 font-medium">{t("mp3quran.author")}</span>
+                </div>
+                <ForwardArrow size={16} silent locale={params.locale} />
+              </div>
+            </div>
+          </Link>
+
+                              {/* Quran Encyclopedia Translations */}
+          <Link 
+            href={t("quranenc.link")} 
+            target="_blank" 
+            className="flex flex-col gap-4 p-4 sm:p-6 rounded-xl bg-white hover:shadow-xl shadow-neutral-200 transition-all duration-300 hover:-translate-y-1"
+          >
+            <div className="flex flex-col">
+              <h4 className="text-lg sm:text-xl font-bold mb-2">{t("Quranpedia.title")}</h4>
+              <p className="text-neutral-600 text-sm sm:text-base font-medium leading-relaxed mb-4">
+                {t("quranenc.description")}
+              </p>
+              <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between text-emerald-900 gap-2">
+                <div className="flex items-center gap-2">
+                  <span className="text-sm text-neutral-500">{t("author")}:</span>
+                  <span className="text-sm text-neutral-500 font-medium">{t("quranenc.author")}</span>
+                </div>
+                <ForwardArrow size={16} silent locale={params.locale} />
+              </div>
+            </div>
+          </Link>
+
         </div>
       </div>
     </section>
