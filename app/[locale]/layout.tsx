6787063ccdf7078dc272d05cbafdd1c64a94b7d6
Amr Bendary <amr.bendary.business@gmail.com>
2025-09-08 21:09:17 +0300

    final fix to social preview

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -7,27 +7,16 @@ import "../globals.css";
 import Navbar from "@/app/components/Navbar";
 import Footer from "../components/Footer";
 import NewsletterPopup from "../components/NewsletterPopup";
-
-
 import GoogleAnalytics from "@/app/components/GoogleAnalytics";
 import PageTracking from "@/app/components/PageTracking";
 import StructuredData from "@/app/components/StructuredData";
 import { SanityLive } from "../sanity/live";
+import { seoData } from "../utils/seoData";
 
 
 export async function generateMetadata({ params: { locale } }: { params: { locale: Locale } }): Promise<Metadata> {
   const messages = await getMessages();
-
-  const meta = {
-    title: locale === 'ar' ? 'إتقان | مجتمع تطوير تقنيات القرآن' : 'ITQAN | Quran Tech Community',
-    description: locale === 'ar' 
-      ? 'نهدف لبناء أكبر مجتمع لتطوير تقنيات القرآن الكريم مفتوحة المصدر وتحسين تجربة الاستخدام لخدمة المسلمين حول العالم'
-      : 'We aim to build the largest community for developing open-source Quran technologies And improve the user experience to serve Muslims around the world.',
-    url: locale === 'ar' ? 'https://itqan.dev/ar/' : 'https://itqan.dev/en/',
-    image: locale === 'ar'
-      ? 'https://opengraph.b-cdn.net/production/images/24f3ccc8-f60a-47ce-a95d-bcc9afdf12e0.png?token=PhACL4wnmnsbd1O7du5h2wtpTICdeSQI0X6flfPqMG0&height=630&width=1200&expires=33286317738'
-      : 'https://opengraph.b-cdn.net/production/images/9a5ba497-a818-439e-8ec7-cd0b39559aaf.png?token=s5M6HyA-3T4CyrSGh22syVxc_WCF6vJx6cMChxImgpU&height=630&width=1200&expires=33286319708'
-  };
+  const meta = seoData[locale as keyof typeof seoData];
 
   return {
     title: {
@@ -119,7 +108,7 @@ export default async function RootLayout({ children, params }: Props) {
         {/* Essential Meta Tags */}
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="theme-color" content="#669B80" />
-<meta name="msapplication-TileColor" content="#669B80" />
+        <meta name="msapplication-TileColor" content="#669B80" />
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         <meta name="apple-mobile-web-app-title" content={locale === 'ar' ? 'إتقان' : 'ITQAN'} />
